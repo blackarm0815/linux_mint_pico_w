5e80a1717db43b47d4601cfe8a1a19e75b5ff9f8
magnus <magnusmichaelson@protonmail.com>
2023-06-01 16:20:44 +0100

    :)

--- setup.sh
+++ setup.sh
@@ -1,18 +1,35 @@
 cd
+
+# install dependencies
 sudo apt install -y build-essential cmake gcc-arm-none-eabi git libnewlib-arm-none-eabi 
+
+# create the code folder
 mkdir code
 cd code
+
+# clone the sdk
 git clone https://github.com/raspberrypi/pico-sdk.git
-git clone https://github.com/raspberrypi/pico-examples.git
+
+# install the sdk submodules
 cd pico-sdk
 git submodule update --init
+
+# clone the examples
+cd ..
+git clone https://github.com/raspberrypi/pico-examples.git
+
+# create the build
 cd ../pico-examples
 mkdir build
 cd build
 export PICO_SDK_PATH=../../pico-sdk/
 cmake -DPICO_BOARD=pico_w ..
+
+# build the basic blink program
 cd pico_w/wifi/blink
 make
+
+# copy to home directory and display
 cp picow_blink.uf2 ~
 echo
 ls -1

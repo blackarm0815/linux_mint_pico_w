142dad1dabb332a3800033f39dd53b035014f07c
magnus <magnusmichaelson@protonmail.com>
2023-06-02 11:26:34 +0100

    :)

--- setup.sh
+++ setup.sh
@@ -1,35 +1,60 @@
-# install dependencies
+
+echo
+echo "clearing path environments"
+echo
+unset PICO_SDK_PATH
+unset PICO_EXTRAS_PATH
+unset PIDO_EXAMPLES_PATH
+
+echo
+echo "installing dependencies"
+echo
 sudo apt install -y build-essential cmake gcc-arm-none-eabi git libnewlib-arm-none-eabi
 
-# create the code folder
+echo
+echo "creating the code folder"
+echo
 cd
 mkdir -p code
 cd code
 
-# clone the sdk
+echo
+echo "cloning the sdk repo"
+echo
 git clone https://github.com/raspberrypi/pico-sdk.git
+export PICO_SDK_PATH=~/code/pico-sdk
 
-# install the sdk submodules
+echo
+echo "installing the sdk submodules"
+echo
 cd pico-sdk
 git submodule update --init
 
-# clone the examples
+echo
+echo "cloning the examples repo"
+echo
 cd ..
 git clone https://github.com/raspberrypi/pico-examples.git
+export PICO_EXTRAS_PATH=~/code/pico-sdk
 
-# create the build
+echo
+echo "creating the build folder"
+echo
 cd pico-examples
 mkdir build
 cd build
 export PICO_SDK_PATH=../../pico-sdk/
 cmake -DPICO_BOARD=pico_w ..
 
-# build the basic blink program
+echo
+echo "building the basic blink program"
+echo
 cd pico_w/wifi/blink
 make
 
-# copy to home directory and display
-cp picow_blink.uf2 ~
 echo
-ls -1
+echo "copying the blink code to home directory"
+echo
+cp picow_blink.uf2 ~
+ls -l ~
 echo
